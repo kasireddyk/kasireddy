b06eda313c9980ca17467d475973da6331e72f36
kasireddyk <153628412+kasireddyk@users.noreply.github.com>
2023-12-19 09:24:44 +0530

    reddy

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+k={1:'kasi','r':'reddy','kasi':'reddy'}
+#print(type(k))
+#print(k.items())     ---------->items means both showing like container
+#print(k.keys()) ----------------> keys means only keys display in given dictionarys
+#print(k.values()) ------------->values only display
+#print(k.get('kasi')) -------------> get means using of call on elements
+
+
+
+
+'''
+dictinaries:
+key value data here like{1:'kasi',2:'reddy'}
+1--> its a data type
+2---> its oonly curly brackets like ex-:{}
+3---> its key should be immutable 
+4--->its values should be mutable its  allowing duplicate values
+keys is not allowing duplicate 
+
+
+
+'''
